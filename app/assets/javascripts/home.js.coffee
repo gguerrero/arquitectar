95be39fb81ea24186062e3b29ee0c210fa5b5fdd
Guillermo Guerrero <g.guerrero.bus@gmail.com>
2013-04-08 01:56:40 +0200

    Add kwicks effects with pictures and animation. Restyle home page, now content text changes when kwick section is hovered. Add locale automation routes. Set up the user agent language as default localization.

---
+++ app/assets/javascripts/home.js.coffee
@@ -0,0 +1,59 @@
+# The easing effect list:
+#   easeInQuad
+#   easeOutQuad
+#   easeInOutQuad
+#   easeInCubic
+#   easeOutCubic
+#   easeInOutCubic
+#   easeInQuart
+#   easeOutQuart
+#   easeInOutQuart
+#   easeInQuint
+#   easeOutQuint
+#   easeInOutQuint
+#   easeInSine
+#   easeOutSine
+#   easeInOutSine
+#   easeInExpo
+#   easeOutExpo
+#   easeInOutExpo
+#   easeInCirc
+#   easeOutCirc
+#   easeInOutCirc
+#   easeInElastic
+#   easeOutElastic
+#   easeInOutElastic
+#   easeInBack
+#   easeOutBack
+#   easeInOutBack
+#   easeInBounce
+#   easeOutBounce
+#   easeInOutBounce
+
+$(document).on 'page:load ready', ->
+  $('#home-sexy-kwicks .kwicks').kwicks
+    size: 323
+    maxSize: 900 # That is minSize = (((323*3)-900) / (nPanels-1))
+    spacing: 0
+    duration: 1200
+    behavior: 'menu'
+    easing: 'easeOutBounce'
+    isVertical: false
+
+  $(".panel-item").mouseleave ->
+    section = $(this)[0].id
+    console.log "Leaving " + section
+
+  $(".panel-item").mouseenter ->
+    section = $(this)[0].id
+
+    $(".panel-sumary", this).css "opacity", 0
+    $(".panel-sumary", this).animate {opacity: 0.8}, 1800
+
+    $(".extra-info > div").each ->
+      if this.id is section
+        $(this).show()
+        $(this).css "opacity", 0
+        $(this).animate {opacity: 1}, 2000
+      else
+        $(this).hide()
--- config/routes.rb
+++ config/routes.rb
@@ -4,52 +4,55 @@ Arquitectar::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root to: 'welcome#index'
+  get '/:locale' => "pages#home"
   root to: "pages#home"
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-  get 'home' => 'pages#home'
-  get 'news' => 'pages#news'
-  get 'collaboration' => 'pages#collaboration'
-  get 'contact' => 'pages#contact'
+  scope "(/:locale)", locale: /en|es|eu/ do
+    # Example of regular route:
+    #   get 'products/:id' => 'catalog#view'
+    get 'home' => 'pages#home'
+    get 'news' => 'pages#news'
+    get 'collaboration' => 'pages#collaboration'
+    get 'contact' => 'pages#contact'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+    # Example of named route that can be invoked with purchase_url(id: product.id)
+    #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-  resources :projects, only: [:index]
+    # Example resource route (maps HTTP verbs to controller actions automatically):
+    #   resources :products
+    resources :projects, only: [:index]
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+    # Example resource route with options:
+    #   resources :products do
+    #     member do
+    #       get 'short'
+    #       post 'toggle'
+    #     end
+    #
+    #     collection do
+    #       get 'sold'
+    #     end
+    #   end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+    # Example resource route with sub-resources:
+    #   resources :products do
+    #     resources :comments, :sales
+    #     resource :seller
+    #   end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+    # Example resource route with more complex sub-resources:
+    #   resources :products do
+    #     resources :comments
+    #     resources :sales do
+    #       get 'recent', on: :collection
+    #     end
+    #   end
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    # Example resource route within a namespace:
+    #   namespace :admin do
+    #     # Directs /admin/products/* to Admin::ProductsController
+    #     # (app/controllers/admin/products_controller.rb)
+    #     resources :products
+    #   end
+  end
 end
