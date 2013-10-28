1d0affcbdb4e8e22e089bf9a6dfa4c5e9f343d30
Guillermo Guerrero <g.guerrero.bus@gmail.com>
2013-10-28 18:59:28 +0100

    Fix :cookie_store symbol name new in Rails4

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Arquitectar::Application.config.session_store :encrypted_cookie_store, key: '_arquitectar_session'
+Arquitectar::Application.config.session_store :cookie_store, key: '_arquitectar_session'
